462ed1687f72069ee470083cc0d1ccbe36108169
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-23 23:35:53 -0700

    reference between test and regular project. first passing test.

--- TDDScratch_Tests/Test1.cs
+++ TDDScratch_Tests/Test1.cs
@@ -1,12 +1,23 @@
-﻿namespace TDDScratch_Tests
+﻿using TDDScratch;
+
+namespace TDDScratch_Tests
 {
     [TestClass]
     public sealed class BasicCalculatorTests
     {
         [TestMethod]
-        public void AddingTwoNumbersToBeFour()
+        public void AdditionOfTwoNumbersTest()
         {
+            // Arrange
+            var calculator = new Calculator();
+            int num1 = 2;
+            int num2 = 3;
 
+            //Act 
+            int result = calculator.Add(num1, num2);
+
+            // Assert
+            Assert.AreEqual(5, result);
         }
     }
 }
